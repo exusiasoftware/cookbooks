6ea84774e0c38d876b98e46bbc5fd571bb59f126
Brian Rahming <admin@exusiasoftware.com>
2020-11-22 11:10:42 -0800

    added nginx

--- exs_laravel/recipes/nginx.rb
+++ exs_laravel/recipes/nginx.rb
@@ -5,14 +5,34 @@
 # Copyright:: 2020, The Authors, All Rights Reserved.
 site_name = 'rahmingtech.link'
 
-
+# package 'epel-release'
 package 'nginx'
 
+directory '/etc/nginx/sites-enabled/' do
+    action :create
+end
+
+directory "/var/www/html/#{site_name}" do
+    owner 'nginx'
+    group 'nginx'
+    mode '0755'
+    recursive true
+    action :create
+end
+
+template "/etc/nginx/nginx.conf" do
+    source 'nginx.conf.erb'
+    action :create 
+end
 
 template "/etc/nginx/sites-enabled/#{site_name}" do
     source 'site_domain.erb'
     action :create 
     variables(site_name: site_name) 
+end
+
+service 'nginx' do
+    action [:enable, :start]
 end
 
 service 'nginx' do
--- exs_laravel/test/integration/default/nginx_test.rb
+++ exs_laravel/test/integration/default/nginx_test.rb
@@ -12,5 +12,10 @@ end
 
 # This is an example test, replace it with your own test.
 describe port(80), :skip do
-  it { should_not be_listening }
+  it { should_be_listening }
+end
+
+
+describe package('nginx') do
+  it { should be_installed }
 end
