a217c950c77c25e7b9b229cc0f99c927af6ede34
Brian Rahming <admin@exusiasoftware.com>
2020-11-24 09:43:09 -0800

    Add Opsworks cookbook

--- exs_opsworks_cookbook/recipes/custom_json.rb
+++ exs_opsworks_cookbook/recipes/custom_json.rb
@@ -2,3 +2,13 @@ Chef::Log.info("********** For customer '#{node['customer-id']}' invoice '#{node
 Chef::Log.info("********** Invoice line number 1 is a '#{node['line-items']['line-1']}'**********")
 Chef::Log.info("********** Invoice line number 2 is a '#{node['line-items']['line-2']}'**********")
 Chef::Log.info("********** Invoice line number 3 is a '#{node['line-items']['line-3']}'**********")
+
+#Advanced, Custom Chef JSON,
+# {
+#     "customer-id": "0123",
+#     "invoice-number": "9876",
+#     "line-items": {
+#     "line-1": "tractor",
+#     "line-2": "passenger car",
+#     "line-3": "trailer"
+# }
